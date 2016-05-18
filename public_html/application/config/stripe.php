7874c57c3c1f1eb59e94f4a89e5bf9b160c038da
James Fair <jamesfair51@hotmail.com>
2016-05-18 13:59:00 -0700

    Create config file for Stripe Service

---
+++ public_html/application/config/stripe.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Stripe API Key
+|--------------------------------------------------------------------------
+|
+| This API key is used for all request to Stripe's API.
+*/
+
+
+ stripe['api_key'] ='sk_test_qt6N389SzRKelhOe6wzGMC0x';
+
+
+?>
+
+
