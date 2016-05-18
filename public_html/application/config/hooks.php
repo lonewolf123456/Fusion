28a4b9f2e29c0796679ea8901b9505be19a13c86
James Fair <jamesfair51@hotmail.com>
2016-05-17 22:00:45 -0700

    Add for Stripe and add pre-controller hook for it

--- public_html/application/config/hooks.php
+++ public_html/application/config/hooks.php
@@ -11,3 +11,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |	https://codeigniter.com/user_guide/general/hooks.html
 |
 */
+
+
+$hook['pre-controller'][] = function(){
+    require_once(APPPATH.'/hooks/stripe/init.php');
+};
